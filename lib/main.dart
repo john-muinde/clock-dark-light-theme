575924951518c02740ba0e977651f4239c5da55f
atrocious-jpg <jhnmuinde@gmail.com>
2023-02-10 23:13:48 +0300

    finished the first ui part of the clock, weather, date time column

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:dark_light_theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'constants/constants.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,62 +21,6 @@ class MyApp extends StatelessWidget {
         isLightTheme: true,
       ).themeData(),
       home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: RadialGradient(
-            center: const Alignment(-0.8, -0.3),
-            radius: 1,
-            colors:
-                ThemeProvider(isLightTheme: true).themeMode().gradientColors!,
-          ),
-        ),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    Text(
-                      DateTime.now().hour.toString(),
-                      style: Theme.of(context).textTheme.displayLarge,
-                    ),
-                    SizedBox(
-                      width: size.width * .2,
-                      child: const Divider(
-                        height: 0,
-                        thickness: 1,
-                        color: AppColors.white,
-                      ),
-                    ),
-                    Text(
-                      DateTime.now().minute.toString(),
-                      style: Theme.of(context).textTheme.displayLarge,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
